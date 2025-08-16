9b89b07b355510f34063f7f32d3b3b64c78ac476
traydee <traydee92@gmail.com>
2025-08-17 00:43:00 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -850,15 +850,7 @@ fetchSubscriptionBtn?.addEventListener('click', (e) => {
 
     const shouldBlock = tablet || isLandscape || webTG; 
     blocker.style.display = shouldBlock ? 'flex' : 'none';
-    // Make the blocker unremovable through devtools
-    if (shouldBlock) {
-      blocker.setAttribute("style", "display: flex !important;");
-      Object.freeze(blocker.style);
-      const observer = new MutationObserver(() => {
-        blocker.setAttribute("style", "display: flex !important;");
-      });
-      observer.observe(blocker, { attributes: true, attributeFilter: ['style'] });
-    }
+    
     document.documentElement.style.overflow = shouldBlock ? 'hidden' : '';
     document.body.style.overflow = shouldBlock ? 'hidden' : '';
 
