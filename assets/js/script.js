007e0e3f020410a144175481456a0c54f29a0402
traydee <traydee92@gmail.com>
2025-08-16 23:02:24 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,6 @@ import { setupGround, updateGround } from "./ground.js";
 import {
   setupCharacter,
   updateCharacter,
-  getCharacterRect,
   setCharacterLose,
   onJump,
 } from "./character.js";
@@ -20,6 +19,7 @@ import { setupSerum, updateSerum, getSerumRects } from "./serum.js";
 const API_BASE_URL = "https://webtop.site";
 const SPEED_SCALE_INCREASE = 0.00001;
 let AUDIO_MUTED = true;
+const getCharacterRect = window.getCharacterRect;
 
 const publicKey = `-----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA6CLTHnd6PG12cxjWPLLD
